0098bc24aa23048a2283cf3fd7625912b91d5ba9
pulpcovers <38575620+pulpcovers@users.noreply.github.com>
2026-02-22 10:33:16 -0500

    Update Date Archived.js
    Added 1.0 code

--- Date Archived.js
+++ Date Archived.js
@@ -1,1 +1,27 @@
+// ==UserScript==
+// @name         Archive.org - Default Sort to Date Archived
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Sets default sort to "Date Archived" on archive.org pages
+// @author       You
+// @match        https://archive.org/details/*
+// @match        https://archive.org/search.php*
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://github.com/pulpcovers/Archive.org---Default-Sort-to-Date-Archived/raw/refs/heads/main/Date%20Archived.js
+// @downloadURL  https://github.com/pulpcovers/Archive.org---Default-Sort-to-Date-Archived/raw/refs/heads/main/Date%20Archived.js
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+
+    // Only add sort parameter if no sort parameter is currently set
+    if (!params.has('sort')) {
+        params.set('sort', '-publicdate');
+        // Replace the current URL without adding to history
+        window.location.replace(url.toString());
+    }
+})();
